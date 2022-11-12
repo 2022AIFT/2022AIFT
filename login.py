cf73d6130b8cb54d78666fbe11e30ba3b0e0d6d5
yej11 <yeji@beex.team>
2022-11-13 00:03:14 +0900

    김예지 파트

--- login.py
+++ login.py
@@ -10,3 +10,28 @@ lastInverse = kiwoom.GetMasterLastPrice('226490')
 
 print(last200)
 print(lastInverse)
+
+# 조건식을 pc로 다운로드
+kiwoom.GetConditionLoad()
+
+# 전체 조건식 리스트 얻기exi
+conditions = kiwoom.GetConditionNameList()
+
+# 0번 조건식에 해당하는 종목 리스트 출력
+condition_index = conditions[0][0]
+condition_name = conditions[0][1]
+codes = kiwoom.SendCondition("0101", condition_name, condition_index, 0)
+
+print(codes)
+
+# ---------매수---------
+
+# 주식계좌
+accounts = kiwoom.GetLoginInfo("ACCNO")
+stock_account = accounts[0]
+
+# 삼성전자, 10주, 시장가주문 매수
+kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, "005930", 10, 0, "03", "")
+
+# ---------매도---------
+kiwoom.SendOrder("시장가매도", "0101", stock_account, 2, "005930", 10, 0, "03", "")
