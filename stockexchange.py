bf69fb146116cdfd92fe95dbc7a31381ef2ae70d
ssyyoonn <105106912+ssyyoonn@users.noreply.github.com>
2022-12-08 19:23:32 +0900

    주식체결정보 실시간 구독
    09.03 주식체결 파트 kodexx 200으로 구현

---
+++ stockexchange.py
@@ -0,0 +1,71 @@
+import sys 
+from PyQt5.QtWidgets import *
+from PyQt5.QAxContainer import *
+import datetime
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Real")
+        self.setGeometry(300, 300, 300, 400)
+
+        btn = QPushButton("Register", self)
+        btn.move(20, 20)
+        btn.clicked.connect(self.btn_clicked)
+
+        btn2 = QPushButton("DisConnect", self)
+        btn2.move(20, 100)
+        btn2.clicked.connect(self.btn2_clicked)
+
+        self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.ocx.OnEventConnect.connect(self._handler_login)
+        self.ocx.OnReceiveRealData.connect(self._handler_real_data)
+        self.CommmConnect()
+
+    def btn_clicked(self):
+                        # 체결 시간, 현재가 구독 신청
+        self.SetRealReg("1000", "069500", "20;10", 0) # 069500: 코덱스200 종목코드
+
+    def btn2_clicked(self):
+        self.DisConnectRealData("1000")
+
+    def CommmConnect(self):
+        self.ocx.dynamicCall("CommConnect()")
+        self.statusBar().showMessage("login 중 ...")
+
+    def _handler_login(self, err_code):
+        if err_code == 0:
+            self.statusBar().showMessage("login 완료")
+
+
+    def _handler_real_data(self, code, real_type, data):
+        if real_type == "주식체결":
+            # 체결 시간 
+            time =  self.GetCommRealData(code, 20)
+            date = datetime.datetime.now().strftime("%Y-%m-%d ")
+            time =  datetime.datetime.strptime(date + time, "%Y-%m-%d %H%M%S")
+            print(time, end=" ")
+
+            # 현재가 
+            price =  self.GetCommRealData(code, 10)
+            print(int(price))
+
+
+    def SetRealReg(self, screen_no, code_list, fid_list, real_type):
+        self.ocx.dynamicCall("SetRealReg(QString, QString, QString, QString)", 
+                              screen_no, code_list, fid_list, real_type)
+
+    def DisConnectRealData(self, screen_no):
+        self.ocx.dynamicCall("DisConnectRealData(QString)", screen_no)
+
+    def GetCommRealData(self, code, fid):
+        data = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, fid) 
+        return data
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
